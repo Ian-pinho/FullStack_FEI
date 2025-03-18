37bcbde38b5d18b051e32c8c23e6ee5c2157c28c
Ian-pinho <ianpinho93@gmail.com>
2025-03-17 22:35:08 -0300

    Exercícios de javaScript

---
+++ LABs/script.js
@@ -0,0 +1,15 @@
+var num1 = 1
+while (num1 < 100){
+    console.log(num1)
+    num1+=2
+}
+var num2 =5
+while (num2<=500){
+    console.log(num2)
+    num2+=5
+}
+var num3 = prompt("Digite um número")
+while (num3 > -1) {
+    console.log(num3)
+    num3-=1
+}
