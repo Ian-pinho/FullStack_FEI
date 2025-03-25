6a0a475439f0e4e89ac50f9afb99e20bf952d13c
Ian-pinho <ianpinho93@gmail.com>
2025-03-24 22:28:10 -0300

    Aula 6 - novo projeto fazendo um jogo de adivinhação de um número aleatório.

--- LABs/script.js
+++ LABs/script.js
@@ -1,15 +1,19 @@
-var num1 = 1
-while (num1 < 100){
-    console.log(num1)
-    num1+=2
-}
-var num2 =5
-while (num2<=500){
-    console.log(num2)
-    num2+=5
-}
-var num3 = prompt("Digite um número")
-while (num3 > -1) {
-    console.log(num3)
-    num3-=1
+let n1 = Math.floor(Math.random()*100)
+console.log(n1)
+function test(){
+    let n2 = parseInt(document.getElementById("num").value)
+    if (n2 < n1){
+        console.log("O número é maior!")
+        document.getElementById("resp").innerHTML = "O número é maior!"
+        document.getElementById("resp").style.setProperty("background-color", "red");
+    }else if (n2 > n1){
+        console.log("O número é menor!")
+        document.getElementById("resp").innerHTML = "O número é menor!"
+        document.getElementById("resp").style.setProperty("background-color", "red");
+    }else{
+        console.log("Você acertou o número!")
+        document.getElementById("resp").innerHTML = "Você acertou o número!"
+        document.getElementById("resp").style.setProperty("background-color", "green");
+
+    }
 }
